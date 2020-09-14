ce6b51dc2a8fe3c4636fc36d1900e679af4deb05
echandiesteban <70446092+echandiesteban@users.noreply.github.com>
2020-09-14 19:57:09 -0400

    Add files via upload
    Bid program, stop with 'n'.

---
+++ Bid_pyhon.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import random
+random.seed(5)
+
+keep_going = '_'
+next_bid = 0
+
+while keep_going != 'n' :
+   next_bid = next_bid + random.randint(1, 10)
+   print('I\'ll bid ${}!'.format(next_bid))
+   print('Continue bidding?', end=' ')
+   keep_going = input()
+
+
+# In[ ]:
+
+
+
+
